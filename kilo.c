bf3adf2a57245a1f3d9eb43c80250c6c3ff0b5d7
Aayush Shukla <aayushshukla@Aayushs-MacBook-Pro.local>
2025-02-11 13:53:14 +0530

    Made modifications so that it clears the screen and repositions the cursor on exit of program

--- kilo.c
+++ kilo.c
@@ -16,6 +16,8 @@
 struct termios orig_termios;
 
 void die(const char *s){
+   write(STDOUT_FILENO,"\x1b[2J",4);
+   write(STDOUT_FILENO,"\x1b[H",3);
    perror(s);
    exit(1);
 }
@@ -64,6 +66,8 @@ void editorProcessKeypress(){
   char c=editorReadKey();
   switch(c){
    case CTRL_KEY('q'):
+    write(STDOUT_FILENO,"\x1b[2J",4);
+    write(STDOUT_FILENO,"\x1b[H",3);
     exit(1);
     break;
    }
