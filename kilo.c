96295a1b68bb77ad0cd33ea1a7e47ce2fc5090ba
Aayush Shukla <aayushshukla@Aayushs-MacBook-Pro.local>
2025-02-14 10:20:11 +0530

    Adding tildes at the beginning of any line that comes after the end of the file being edited

--- kilo.c
+++ kilo.c
@@ -52,9 +52,22 @@ char editorReadKey(){
    return c;
 }
 /*** output ***/
-//"/x1b" is the escape character
+
+void editorDrawRows(){
+  int y;
+  for(y=0;y<24;y++){
+    write(STDOUT_FILENO,"~\r\n",3);
+    }
+}
+
+//"\x1b" is the escape character
+
 void editorRefreshScreen(){
    write(STDOUT_FILENO,"\x1b[2J",4);
+   write(STDOUT_FILENO,"\x1b[H",3);
+
+   editorDrawRows();
+
    write(STDOUT_FILENO,"\x1b[H",3);
 }
 
