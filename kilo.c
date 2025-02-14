8143ac256e0e52e547563cb967cf4f6bbea35c13
Aayush Shukla <aayushshukla@Aayushs-MacBook-Pro.local>
2025-02-14 10:46:48 +0530

    Getting the size of the window calling ioctl() with the TIOCGWINSZ request(easy way)

--- kilo.c
+++ kilo.c
@@ -5,6 +5,7 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<errno.h>
+#include<sys/ioctl.h>
 
 /*** defines ***/
 
@@ -14,6 +15,8 @@
 
 /*** data ***/
 struct editorConfig {
+ int screenrows;
+ int screencols;
  struct termios orig_termios;
 };
 
@@ -55,6 +58,19 @@ char editorReadKey(){
     }
    return c;
 }
+
+int getWindowSize(int *rows,int *cols){
+  struct winsize ws;
+  
+  if(ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws)== -1 || ws.ws_col == 0) return -1;
+  else{
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+   }
+}
+
+
 /*** output ***/
 
 void editorDrawRows(){
@@ -92,8 +108,14 @@ void editorProcessKeypress(){
 
 
 /*** init ***/
+
+void initEditor(){
+  if(getWindowSize(&E.screenrows, &E.screencols)== -1) die("getWindowSize");
+}
+
 int main(){
   enableRawMode();
+  initEditor();
 
   while(1){
    editorRefreshScreen();
