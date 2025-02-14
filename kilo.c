93fd1013f0a8f41b2f71d1cfdde070f767f7ed04
Aayush Shukla <aayushshukla@Aayushs-MacBook-Pro.local>
2025-02-14 10:32:46 +0530

    Added a global state struct which will store the state of editor;which we will use to store the width and height of the terminal

--- kilo.c
+++ kilo.c
@@ -13,8 +13,13 @@
 
 
 /*** data ***/
-struct termios orig_termios;
+struct editorConfig {
+ struct termios orig_termios;
+};
 
+struct editorConfig E;
+
+/*** terminal ***/
 void die(const char *s){
    write(STDOUT_FILENO,"\x1b[2J",4);
    write(STDOUT_FILENO,"\x1b[H",3);
@@ -22,16 +27,15 @@ void die(const char *s){
    exit(1);
 }
 
-/*** terminal ***/
 void disableRawMode(){
- if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios)==-1)die("tcsetattr");
+ if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&E.orig_termios)==-1)die("tcsetattr");
 }
 
 void enableRawMode(){
-  if(tcgetattr(STDIN_FILENO,&orig_termios)==-1)die("tcgetattr");
+  if(tcgetattr(STDIN_FILENO,&E.orig_termios)==-1)die("tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw_struct= orig_termios;
+  struct termios raw_struct= E.orig_termios;
   raw_struct.c_iflag &=~(BRKINT|ICRNL|IXON|INPCK|ISTRIP);
   raw_struct.c_oflag &=~(OPOST);
   raw_struct.c_cflag |=(CS8);
